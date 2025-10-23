2eb8e968584ecd0736e17e8d43698855d5965afd
root <lizongzai@gmail.com>
2025-10-23 17:13:25 +0800

    写入文件的两种方法：覆盖写入和追加内容写入

--- day21/file_operation_sample03.py
+++ day21/file_operation_sample03.py
@@ -1,3 +1,10 @@
+"""
+Version: 1.0
+Author: 李小婕
+Description: 写入文件的两种方法：覆盖写入和追加内容写入
+Date: 2025-10-23
+"""
+
 # 写入文件（覆盖）
 with open('example.txt', 'w', encoding='utf-8') as file:
     file.write('Hello, World!\n')
